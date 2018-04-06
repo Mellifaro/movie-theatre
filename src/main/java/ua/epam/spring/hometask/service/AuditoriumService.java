42e6c08504e118b1bb0913e464885d2fd5c903f4
Viktor Skapoushchenko <viktor_skapoushchenko@epam.com>
2018-04-06 15:24:15 +0300

    Corrected comments in Auditorium service

--- src/main/java/ua/epam/spring/hometask/service/AuditoriumService.java
+++ src/main/java/ua/epam/spring/hometask/service/AuditoriumService.java
@@ -14,16 +14,13 @@ public interface AuditoriumService {
 
     /**
      * Getting all auditoriums from the system
-     * 
      * @return set of all auditoriums
      */
     public @Nonnull Set<Auditorium> getAll();
 
     /**
      * Finding auditorium by name
-     * 
-     * @param name
-     *            Name of the auditorium
+     * @param name Name of the auditorium
      * @return found auditorium or <code>null</code>
      */
     public @Nullable Auditorium getByName(@Nonnull String name);
