dbf6a6362f95b68689e11f42570468c04c211348
Viktor Skapoushchenko <viktor_skapoushchenko@epam.com>
2018-04-30 12:37:38 +0300

    Started booking functionality

--- src/main/java/ua/epam/spring/hometask/controller/EventController.java
+++ src/main/java/ua/epam/spring/hometask/controller/EventController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ua.epam.spring.hometask.domain.Event;
 import ua.epam.spring.hometask.service.event.EventService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -25,5 +28,12 @@ public class EventController {
         List<Event> users = (List<Event>)eventService.getAll();
         modelMap.put("events", users);
         return "events";
+    }
+
+    @GetMapping(value = "/{id}/datetime/{datetime}")
+    public String getEventByIdAndDate(ModelMap modelMap, @PathVariable("id") long id, @PathVariable("datetime") String dateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime eventTime = LocalDateTime.parse(dateTime, formatter);
+        return "eventInfo";
     }
 }
